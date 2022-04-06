d3ec0e90b8f1b99e601ec20edc972c89e3dd129f
Adej0la <dejoladaniel@gmail.com>
2022-04-06 16:35:03 +0100

    feat: add erc721 smart contract also added some functionality to nestcoin erc20 contract

---
+++ contracts/CreateNFT.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    /**
+    * @dev mint event for frontend consumption. 
+    */
+    event NFTMinted(address indexed _sender, uint256 _tokenId);
+
+    constructor() ERC721("NestCoin", "NCN") {}
+
+    /**
+    * @dev Mints the token, adds a unique ID and
+    * sets the token metadata.
+    */
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        uri = string(abi.encodePacked("{'name': 'Subaru Boy ", tokenId , "', 'image': 'https://ipfs.io/ipfs/ QmPLUFGapx7QCMLKcBXSjmRj9nv1DVMbaqhCoqosmuNENP', 'description': 'A collection of super rare Subaru boys.'}"));
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        emit NFTMinted(to, tokenId);
+    }
+
+    /**
+    * @dev destroys tokenId
+    */
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+   /**
+   * @dev set tokenURI for tokenId.
+   */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
