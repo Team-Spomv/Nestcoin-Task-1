6de0a14ecfb67569e1f2e1d152473a031b3c0952
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-08 09:53:24 +0100

    trying to fix connecting with metawallet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+require('@openzeppelin/hardhat-upgrades');
 
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
   networks: {
-    hardhat: {
-      chainId: 31337,
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/427069041f66480c93be73ccb223693e", // <---- YOUR INFURA ID! (or it won't work)
     },
-  },
+    // hardhat: {
+    //   chainId: 1337
+    // }
+  }
 };
