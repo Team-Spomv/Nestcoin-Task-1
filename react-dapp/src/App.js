370006ccf167149a5f659dcb183972fa54d8d3e3
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-08 13:09:18 +0100

    fixed router issue

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -1,16 +1,15 @@
-
-import './App.css';
-import Header from './components/Header';
-import UploadTokens from './components/UploadTokens';
-import {Menu, Layout, Breadcrumb, Icon, Divider, Typography} from 'antd';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import 'antd/dist/antd.css';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import UploadTokens from "./components/UploadTokens";
+import { Menu, Layout, Breadcrumb, Icon, Divider, Typography } from "antd";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import "antd/dist/antd.css";
+import React, { useState, useEffect } from "react";
+import Marketplace from "./components/Marketplace";
 
 const { Title } = Typography;
 
 function App() {
-
   const [route, setRoute] = useState();
 
   useEffect(() => {
@@ -20,44 +19,48 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
-      <Menu style={{ textAlign: "center" }} selectedKeys={[route]} mode="horizontal">
-          <Menu.Item key="/">
-            <Link
-              onClick={() => {
-                setRoute("/");
-              }}
-              to="/"
-            >
-              NestCoin Tokens
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/marketplace">
-            <Link
-              onClick={() => {
-                setRoute("/marketplace");
-              }}
-              to="/marketplace"
-            >
-              Marketplace
-            </Link>
-          </Menu.Item>
-        </Menu>
+      
 
+      <BrowserRouter>
+      <Menu
+        style={{ textAlign: "center" }}
+        selectedKeys={[route]}
+        mode="horizontal"
+      >
+        <Menu.Item key="/">
+          <Link
+            onClick={() => {
+              setRoute("/");
+            }}
+            to="/"
+          >
+            NestCoin Tokens
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="/marketplace">
+          <Link
+            onClick={() => {
+              setRoute("/marketplace");
+            }}
+            to="/marketplace"
+          >
+            Marketplace
+          </Link>
+        </Menu.Item>
+      </Menu>
         <Switch>
           <Route exact path="/">
-            <div style={{ padding: '2rem'}}>
+            <div style={{ padding: "2rem" }}>
               <Title>Transfer Tokens</Title>
-          <UploadTokens />
-          </div>
+              <UploadTokens />
+            </div>
             <Divider />
-
           </Route>
           <Route path="/marketplace">
-            
+            <Marketplace />
           </Route>
         </Switch>
-        </Router>
+      </BrowserRouter>
     </div>
   );
 }
