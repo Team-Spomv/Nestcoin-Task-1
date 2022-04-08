482f511832fde9a2db96661659cc11ca4a5b7df5
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-04-08 14:35:21 +0100

    Update CreateNFT.sol

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -5,10 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./NestCoin.sol";
 
 contract MyToken is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-
+    NestCoin public nestCoin;
     Counters.Counter private _tokenIdCounter;
 
     /**
@@ -22,13 +23,14 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
     * @dev Mints the token, adds a unique ID and
     * sets the token metadata.
     */
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory uri) public returns(uint256) {
         uint256 tokenId = _tokenIdCounter.current();
         uri = string(abi.encodePacked("{'name': 'Subaru Boy ", tokenId , "', 'image': 'https://ipfs.io/ipfs/ QmPLUFGapx7QCMLKcBXSjmRj9nv1DVMbaqhCoqosmuNENP', 'description': 'A collection of super rare Subaru boys.'}"));
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
         emit NFTMinted(to, tokenId);
+        return tokenId;
     }
 
     /**
@@ -48,5 +50,11 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function awardItem(address minter, string memory metadataURI, uint256 price){
+        nestCoin.transferFrom(msg.sender, address (this), price);
+        uint256 newToken = safeMint(minter, matadataURI);
+
     }
 }
