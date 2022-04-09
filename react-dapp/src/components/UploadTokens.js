c9ad77acf4b99184f9537b3243c4757bfb81853b
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-04-09 14:22:13 +0100

    setup check to disallow transfer to more than 200 accounts | setup other error allerts

--- react-dapp/src/components/UploadTokens.js
+++ react-dapp/src/components/UploadTokens.js
@@ -1,11 +1,11 @@
-import { Button, Row, Col, notification } from "antd";
+import { Button, Row, Col, notification, Input } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import { ExcelRenderer } from "react-excel-renderer";
 
 const openNotification = (type, description) => {
   notification[type]({
-    message: (type==="success") ? type.toUpperCase() : "Loading",
+    message: type === "warning" ? "LOADING..." : type.toUpperCase(),
     description,
   });
 };
@@ -13,6 +13,8 @@ const openNotification = (type, description) => {
 const UploadTokens = (props) => {
   const [fileName, setFileName] = useState("");
   const [state, setState] = useState({});
+  // Token Amount to be sent
+  const [amountToSend, setAmountToSend] = useState();
 
   const fileHandler = (event) => {
     let fileObj = event.target.files[0];
@@ -34,12 +36,13 @@ const UploadTokens = (props) => {
 
   // send bulk transfer transaction
   const sendTransaction = async (address) => {
-
     try {
-
       // loading alert to show transaction is being sent
       openNotification("warning", "Loading...");
-      const tx = await props.contract.batchTransfer(address, props.ethers.utils.parseEther('0.005')._hex);
+      const tx = await props.contract.batchTransfer(
+        address,
+        props.ethers.utils.parseEther(String(amountToSend))._hex
+      );
       tx.wait();
 
       // alert success message
@@ -51,7 +54,6 @@ const UploadTokens = (props) => {
 
   return (
     <>
-
       <div className="ant-upload ant-upload-drag">
         <label className="custom-file-upload">
           <p className="ant-upload-drag-icon">
@@ -75,16 +77,45 @@ const UploadTokens = (props) => {
       </div>
       <span>{fileName}</span>
 
+      <div
+        value={`${amountToSend} `}
+        suffix="NestCoin(NSC)"
+        onChange={(e) => {
+          setAmountToSend(e.target.value);
+          console.log(amountToSend);
+        }}
+        style={{ margin: "1rem 0" }}
+      >
+        <Input placeholder="NestCoin(NSC) Amount to be sent" />
+      </div>
+
       <div style={{ padding: 8 }}>
-            <Button
-              type={"primary"}
-              onClick={() => {
-                sendTransaction(state.rows.map(data => data[1] !== undefined ? data[1] : null))
-              }}
-            >
-              Send Tokens
-            </Button>
-          </div>
+        <Button
+          type={"primary"}
+          onClick={() => {
+            // send bulk transfer transaction
+            !state.rows
+              ? openNotification("error", "Please upload a file")
+              : !amountToSend
+              ? openNotification(
+                  "error",
+                  "Please enter the amount you want to send"
+                )
+              : state.rows.length > 200
+              ? openNotification(
+                  "error",
+                  "Maximum number of addresses exceeded,        \n \n Reduce number to 200"
+                )
+              : sendTransaction(
+                  state.rows.map((data) =>
+                    data[1] !== undefined ? data[1] : null
+                  )
+                );
+          }}
+        >
+          Send Tokens
+        </Button>
+      </div>
 
       <Row>
         <Col span={12} className="">
